129924509ffa857a3aae786b366757aff2098a72
Jakkraphop Pengchan <jakkph@gmail.com>
2023-08-05 16:38:05 +0700

    crisp chat env edited

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,19 @@
+import { LandingContent } from '@/components/landing-content'
+import { LandingHero } from '@/components/landing-hero'
+import { LandingNavbar } from '@/components/landing-navbar'
+import { Button } from '@/components/ui/button'
+import { LayoutDashboard, LogIn, User } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const LandingPage = () => {
+  return (
+    <div className='h-full'>
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+  )
+}
+
+export default LandingPage
